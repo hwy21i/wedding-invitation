08e2a41192ae869707c6e6283162986903489109
Your Name <hawariyayohannes52@gmail.com>
2026-05-09 00:01:46 +0300

    backend ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,26 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
 import { ensureFile } from "./utils/fileDb.js";
+import cors from "cors";
+import express from "express";
+const PORT = process.env.PORT || 5000;
 
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 async function startServer() {
   await ensureFile(env.dataFile);
 
