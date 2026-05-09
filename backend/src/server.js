38b18b1998b88c608f7b432ceaab8afa6c1b8504
Your Name <hawariyayohannes52@gmail.com>
2026-05-09 10:16:42 +0300

    removed vercel config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,6 @@
 import app from "./app.js";
 import { env } from "./config/env.js";
 import { ensureFile } from "./utils/fileDb.js";
-import express from "express";
-import cors from "cors";
-
-const app = express();
-
-const PORT = process.env.PORT || 5000;
-app.use(cors());
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-app.get("/", (req, res) => {
-  res.send("Backend running");
-});
 
 async function startServer() {
   await ensureFile(env.dataFile);
